908c2f0ae1608ce1ac8f177f43020a62692719a9
JohnBN1970 <308669014+JohnBN1970@users.noreply.github.com>
2026-08-18 10:20:14 +0200

    Enforce project access on finance cockpit

--- web/modules/custom/brebo_finance/src/Controller/FinancialCockpitController.php
+++ web/modules/custom/brebo_finance/src/Controller/FinancialCockpitController.php
@@ -6,9 +6,11 @@ namespace Drupal\brebo_finance\Controller;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\brebo_finance\Service\FinancialCockpitBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
@@ -19,12 +21,14 @@ final class FinancialCockpitController implements ContainerInjectionInterface {
   public function __construct(
     private readonly FinancialCockpitBuilder $cockpitBuilder,
     private readonly EntityTypeManagerInterface $entityTypeManager,
+    private readonly AccountProxyInterface $currentUser,
   ) {}
 
   public static function create(ContainerInterface $container): self {
     return new self(
       $container->get('brebo_finance.financial_cockpit_builder'),
       $container->get('entity_type.manager'),
+      $container->get('current_user'),
     );
   }
 
@@ -35,6 +39,9 @@ final class FinancialCockpitController implements ContainerInjectionInterface {
       ->load($projectId);
     if ($project === NULL) {
       throw new NotFoundHttpException('BREBO project does not exist.');
+    }
+    if (!$project->access('view', $this->currentUser)) {
+      throw new AccessDeniedHttpException('No access to this BREBO project.');
     }
 
     $response = new JsonResponse($this->cockpitBuilder->build($projectId));
