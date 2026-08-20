7e8d763d9a080c00c67e739344a552bd6d4d229e
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-08-20 13:51:47 +0200

    Add cross-domain My Work Queue API controller

---
+++ web/modules/custom/brebo_finance/src/Controller/MyFinanceActionsController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+namespace Drupal\brebo_finance\Controller;
+use Drupal\brebo_finance\Service\PersonalWorkQueue;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+final class MyFinanceActionsController extends ControllerBase {
+ public function __construct(private readonly PersonalWorkQueue $workQueue) {}
+ public static function create(ContainerInterface $container):static {return new static($container->get('brebo_finance.personal_work_queue'));}
+ public function view():JsonResponse {$uid=(int)$this->currentUser()->id();$data=$this->workQueue->build($uid);$data['owner']=['uid'=>$uid,'name'=>$this->currentUser()->getDisplayName()];return new JsonResponse($data,200,['Cache-Control'=>'private, no-store']);}
+}
