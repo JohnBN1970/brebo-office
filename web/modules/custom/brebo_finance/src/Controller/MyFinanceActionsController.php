d49820101897b8c332995cb3374343fa0985b9f5
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-08-20 11:15:35 +0200

    Switch My Work Queue API to cross-domain aggregator

--- web/modules/custom/brebo_finance/src/Controller/MyFinanceActionsController.php
+++ web/modules/custom/brebo_finance/src/Controller/MyFinanceActionsController.php
@@ -4,22 +4,22 @@ declare(strict_types=1);
 
 namespace Drupal\brebo_finance\Controller;
 
-use Drupal\brebo_finance\Service\InvoiceBlockerActionManager;
+use Drupal\brebo_finance\Service\PersonalWorkQueue;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-/** Personal work queue API for the signed-in user. */
+/** Personal BREBO Office work queue API for the signed-in user. */
 final class MyFinanceActionsController extends ControllerBase {
-  public function __construct(private readonly InvoiceBlockerActionManager $actions) {}
+  public function __construct(private readonly PersonalWorkQueue $workQueue) {}
 
   public static function create(ContainerInterface $container): static {
-    return new static($container->get('brebo_finance.invoice_blocker_action_manager'));
+    return new static($container->get('brebo_finance.personal_work_queue'));
   }
 
   public function view(): JsonResponse {
     $uid = (int) $this->currentUser()->id();
-    $data = $this->actions->ownerSummary($uid);
+    $data = $this->workQueue->build($uid);
     $data['owner'] = ['uid' => $uid, 'name' => $this->currentUser()->getDisplayName()];
     return new JsonResponse($data, 200, ['Cache-Control' => 'private, no-store']);
   }
