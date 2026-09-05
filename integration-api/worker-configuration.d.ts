554978acab8c6f358f48768c80e6aa5e9f35adc9
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-05 23:12:24 +0200

    Werk Worker bindings bij voor projectpublicatie

--- integration-api/worker-configuration.d.ts
+++ integration-api/worker-configuration.d.ts
@@ -1,8 +1,9 @@
 /* eslint-disable */
 // Generated shape for Wrangler bindings. Regenerate with `wrangler types --include-runtime=false` before deployment.
 interface __BaseEnv_Env {
-	API_VERSION: "1.1.0-draft";
-	WORKER_VERSION: "0.2.0";
+	API_VERSION: "1.2.0-draft";
+	WORKER_VERSION: "0.3.0";
+	OFFICE_PUBLICATION_BASE_URL: "https://sboffice.brebobv.nl";
 	OPENAI_MODEL: "gpt-5-mini";
 	MAX_INPUT_CHARS: "12000";
 	MAX_BODY_BYTES: "32768";
@@ -27,11 +28,11 @@ interface __BaseEnv_Env {
 	SALES_INVOICE_DISPATCH_GUARD: DurableObjectNamespace<import("./src/index").SalesInvoiceDispatchGuard>;
 }
 declare namespace Cloudflare {
-	interface GlobalProps { mainModule: typeof import("./src/index"); durableNamespaces: "ReplayGuard" | "UsageGuard" | "SalesInvoiceDispatchGuard"; }
+	interface GlobalProps { mainModule: typeof import("./src/router"); durableNamespaces: "ReplayGuard" | "UsageGuard" | "SalesInvoiceDispatchGuard"; }
 	interface Env extends __BaseEnv_Env {}
 }
 interface Env extends __BaseEnv_Env {}
 type StringifyValues<EnvType extends Record<string, unknown>> = { [Binding in keyof EnvType]: EnvType[Binding] extends string ? EnvType[Binding] : string; };
 declare namespace NodeJS {
-	interface ProcessEnv extends StringifyValues<Pick<Cloudflare.Env, "API_VERSION" | "WORKER_VERSION" | "OPENAI_MODEL" | "MAX_INPUT_CHARS" | "MAX_BODY_BYTES" | "MAX_CLOCK_SKEW_SECONDS" | "REPLAY_TTL_SECONDS" | "OUTBOUND_IDEMPOTENCY_TTL_SECONDS" | "OPENAI_TIMEOUT_MS" | "OPENAI_MAX_OUTPUT_TOKENS" | "RATE_WINDOW_SECONDS" | "MAX_ANALYSES_PER_WINDOW" | "MONTHLY_TOKEN_BUDGET" | "MONEYBIRD_ADMINISTRATION_ID" | "MONEYBIRD_ACCESS_TOKEN" | "ABN_AMRO_PAYMENT_ENDPOINT" | "ABN_AMRO_INSIGHT_BASE_URL" | "ABN_AMRO_ACCESS_TOKEN" | "ABN_AMRO_API_KEY" | "OPENAI_API_KEY" | "BREBO_SHARED_SECRET">> {}
+	interface ProcessEnv extends StringifyValues<Pick<Cloudflare.Env, "API_VERSION" | "WORKER_VERSION" | "OFFICE_PUBLICATION_BASE_URL" | "OPENAI_MODEL" | "MAX_INPUT_CHARS" | "MAX_BODY_BYTES" | "MAX_CLOCK_SKEW_SECONDS" | "REPLAY_TTL_SECONDS" | "OUTBOUND_IDEMPOTENCY_TTL_SECONDS" | "OPENAI_TIMEOUT_MS" | "OPENAI_MAX_OUTPUT_TOKENS" | "RATE_WINDOW_SECONDS" | "MAX_ANALYSES_PER_WINDOW" | "MONTHLY_TOKEN_BUDGET" | "MONEYBIRD_ADMINISTRATION_ID" | "MONEYBIRD_ACCESS_TOKEN" | "ABN_AMRO_PAYMENT_ENDPOINT" | "ABN_AMRO_INSIGHT_BASE_URL" | "ABN_AMRO_ACCESS_TOKEN" | "ABN_AMRO_API_KEY" | "OPENAI_API_KEY" | "BREBO_SHARED_SECRET">> {}
 }
