eb97ee10fdfe06a1d622126b011798f49add09a9
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:08:51 +0200

    Measure: CI-please test

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureCiPleaseTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** CI-please marker. */
+final class MeasureCiPleaseTest extends TestCase {
+
+  public function testCi(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
