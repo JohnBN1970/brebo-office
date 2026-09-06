8ddf6f60c6ca1cc5812e7d5ca30562586334470f
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 23:54:25 +0200

    Measure: centralize first scanner research thresholds

---
+++ ios/MeasurePOC/Sources/ScanResearchConfig.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum ScanResearchConfig {
+    static let minimumDepthFrames = 30
+    static let minimumMeshAnchors = 1
+    static let minimumPlaneSamples = 50
+    static let maximumPlaneResidualMm = 10.0
+    static let repeatedPhysicalScans = 10
+}
