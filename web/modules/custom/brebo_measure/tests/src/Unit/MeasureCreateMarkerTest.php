c2af4fac5386ee43b9088a4bd81bae88204f5b5f
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:01:53 +0200

    Measure: create marker test

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureCreateMarkerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** Create marker. */
+final class MeasureCreateMarkerTest extends TestCase {
+
+  public function testCreatePr(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
