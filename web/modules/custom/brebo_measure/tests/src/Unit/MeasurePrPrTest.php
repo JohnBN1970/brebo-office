cc28dd5ee0c5aef82ee9b845e74b72c168d82db7
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:04:58 +0200

    Measure: PR marker final test

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasurePrPrTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** PR marker final. */
+final class MeasurePrPrTest extends TestCase {
+
+  public function testPr(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
