a0f9c3399b39af97ae854a2af720feabdb03317d
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:05:30 +0200

    Measure: no-further test

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureNoFurtherTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** No-further marker. */
+final class MeasureNoFurtherTest extends TestCase {
+
+  public function testNoFurther(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
