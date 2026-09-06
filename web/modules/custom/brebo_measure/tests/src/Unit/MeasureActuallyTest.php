d09cf0bcf5d4248a11f075922438f45a34e5b960
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:03:09 +0200

    Measure: actually-stop test

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureActuallyTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** Actually-stop marker. */
+final class MeasureActuallyTest extends TestCase {
+
+  public function testStop(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
