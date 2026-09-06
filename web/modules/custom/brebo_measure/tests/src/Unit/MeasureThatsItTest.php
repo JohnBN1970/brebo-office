2122c1c468897056c137af8e70eb4e5e4b3bce04
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:04:50 +0200

    Measure: thats-it test

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureThatsItTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** That's-it marker. */
+final class MeasureThatsItTest extends TestCase {
+
+  public function testIt(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
