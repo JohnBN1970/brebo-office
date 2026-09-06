e1de941f5592433fa751aff2aee85ca1d3062f1e
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 20:59:29 +0200

    Measure: enough marker

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureEnoughTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** Enough marker. */
+final class MeasureEnoughTest extends TestCase {
+
+  public function testCiNext(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
