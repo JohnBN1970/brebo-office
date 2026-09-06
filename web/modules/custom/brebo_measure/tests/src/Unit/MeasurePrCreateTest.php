e3b45070513485cfd7e0ad7e189c0bd689a5f3ca
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:01:01 +0200

    Measure: create PR test marker

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasurePrCreateTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** Create PR marker. */
+final class MeasurePrCreateTest extends TestCase {
+
+  public function testCreate(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
