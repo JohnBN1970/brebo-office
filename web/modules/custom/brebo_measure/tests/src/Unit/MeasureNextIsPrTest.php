52ace259e0c14df6a1751cec91de1e0f56ccb974
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:04:12 +0200

    Measure: next-is-PR test

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureNextIsPrTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** Next-is-PR marker. */
+final class MeasureNextIsPrTest extends TestCase {
+
+  public function testPr(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
