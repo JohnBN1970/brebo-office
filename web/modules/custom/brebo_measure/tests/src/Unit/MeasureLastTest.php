8e4b929ee110b00f8e22d93fe14ac33fec62f150
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 20:58:34 +0200

    Measure: last pre-PR test marker

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureLastTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** Last pre-PR marker. */
+final class MeasureLastTest extends TestCase {
+
+  public function testLast(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
