16160432cf3d278bda01810466c0b41b7b0b3195
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:03:49 +0200

    Measure: literally-last test

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureLiterallyLastTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** Literally-last marker. */
+final class MeasureLiterallyLastTest extends TestCase {
+
+  public function testLast(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
