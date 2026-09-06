25988aa0c1242a4e4c93e366e835fede52d898ca
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:02:02 +0200

    Measure: last-last test

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureLastLastTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** Last-last marker. */
+final class MeasureLastLastTest extends TestCase {
+
+  public function testLastLast(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
