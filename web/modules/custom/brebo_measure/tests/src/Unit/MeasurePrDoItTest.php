d3f564d7e81a6cb68aec7ea84e912ba27a626099
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:07:38 +0200

    Measure: do-it test

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasurePrDoItTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** Do-it marker. */
+final class MeasurePrDoItTest extends TestCase {
+
+  public function testDoIt(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
