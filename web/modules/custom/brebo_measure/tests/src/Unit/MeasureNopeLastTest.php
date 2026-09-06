ec504715fabd7844a8b021149b4562f62e9dfdd3
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 21:00:46 +0200

    Measure: no-more-commits marker

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureNopeLastTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** No-more-commits marker. */
+final class MeasureNopeLastTest extends TestCase {
+
+  public function testCiNow(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
