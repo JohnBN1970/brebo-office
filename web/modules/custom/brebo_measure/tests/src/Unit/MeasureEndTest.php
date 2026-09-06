3bbc48a977e42c4c4d9018603bc4646afd0f17cf
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-06 20:56:52 +0200

    Measure: add implementation end marker

---
+++ web/modules/custom/brebo_measure/tests/src/Unit/MeasureEndTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\brebo_measure\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/** Implementation end marker. */
+final class MeasureEndTest extends TestCase {
+
+  public function testAwaitingCi(): void {
+    self::assertTrue(TRUE);
+  }
+
+}
