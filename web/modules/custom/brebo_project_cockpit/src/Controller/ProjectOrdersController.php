0ba0911b316d3671cd709ec2bc37ea31517a117b
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-15 16:23:13 +0200

    Expose AI order draft action in project Orders

--- web/modules/custom/brebo_project_cockpit/src/Controller/ProjectOrdersController.php
+++ web/modules/custom/brebo_project_cockpit/src/Controller/ProjectOrdersController.php
@@ -84,6 +84,12 @@ final class ProjectOrdersController extends ControllerBase {
           '#url' => Url::fromRoute('brebo_project_cockpit.order_add', ['node' => $projectId]),
           '#attributes' => ['class' => ['button', 'button--primary']],
         ],
+        'ai_outgoing' => [
+          '#type' => 'link',
+          '#title' => $this->t('AI order voorbereiden'),
+          '#url' => Url::fromRoute('brebo_project_cockpit.order_ai_draft', ['node' => $projectId]),
+          '#attributes' => ['class' => ['button']],
+        ],
         'contracts' => [
           '#type' => 'link',
           '#title' => $this->t('Opdrachtbasis / contracten'),
